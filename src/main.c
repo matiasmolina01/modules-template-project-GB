de1c31ed405b94e7454c2f64471b01df12c2e021
martiiponsaa <marti.ponsa01@estudiant.upf.edu>
2026-01-13 21:11:53 +0100

    fixed some issues on main.c for it to compile

--- src/main.c
+++ src/main.c
@@ -13,31 +13,31 @@ int main(int argc, char *argv[]) {
     fprintf(ofile, "Finished module_args!!\n");
 
     fprintf(ofile, "Starting module_classifier ...\n");
-    classifier(argc, argv);
+    classifier(n);
     fprintf(ofile, "Finished module_classifier!!\n");
 
     fprintf(ofile, "Starting module_input_handler ...\n");
-    input_handler(argc, argv);
+    input_handler(n);
     fprintf(ofile, "Finished module_input_handler!!\n");
 
     fprintf(ofile, "Starting module_output_handler ...\n");
-    output_handler(argc, argv);
+    output_handler(n);
     fprintf(ofile, "Finished module_output_handler!!\n");
     
     fprintf(ofile, "Starting module_recursivity_handler ...\n");
-    recursivity_handler(argc, argv);
+    recursivity_handler(n);
     fprintf(ofile, "Finished module_recursivity_handler!!\n");
 
     fprintf(ofile, "Starting module_symbol_resolver ...\n");
-    module_symbol_resolver(argc, argv);
+    symbol_resolver(n);
     fprintf(ofile, "Finished module_symbol_resolver!!\n");
 
     fprintf(ofile, "Starting module_symbol_table ...\n");
-    module_symbol_table(argc, argv);
+    symbol_table(n);
     fprintf(ofile, "Finished module_symbol_table!!\n");
 
     fprintf(ofile, "Starting module_text_normalizer ...\n");
-    module_text_normalizer(argc, argv);
+    text_normalizer(n);
     fprintf(ofile, "Finished module_text_normalizer!!\n");
 
     printf("All modules executed successfully!\n\n");
