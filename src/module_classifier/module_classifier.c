775022ef2d55966ee8e2ff41e32a8c94f22a8905
FrancOlano <olanofranco1@gmail.com>
2026-01-14 00:32:10 +0100

    upload Classfier module, initial template.

---
+++ src/module_classifier/module_classifier.c
@@ -0,0 +1,19 @@
+/*
+ * -----------------------------------------------------------------------------
+ * module_output_handler.c 
+ *
+ * Usage:
+ *     Used as a "detector" to detect when the input file has comments or directive
+ *
+ * Status:
+ *     Initial development — logic not yet implemented.
+ *
+ * Author: [Franco Olano Melo]
+ * -----------------------------------------------------------------------------
+ */
+
+#include "./module_classifier.h"
+
+int classifier(int n) {
+    printf("Classifier module, still to be implemented\n");
+}
---
+++ src/module_classifier/module_classifier.h
@@ -0,0 +1,9 @@
+#ifndef MODULE_CLASSIFIER_H
+#define MODULE_CLASSIFIER_H
+
+#include "../main.h"
+
+int classifier(int n);
+
+#endif
+ 
