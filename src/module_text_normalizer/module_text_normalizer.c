904e38c9cdbe5ebf64114ea9d4978c57e145823b
ath3n4 <april.anna.vas@gmail.com>
2026-01-14 00:26:04 +0100

    Added text normalizer module

---
+++ src/module_text_normalizer/module_text_normalizer.c
@@ -0,0 +1,19 @@
+/*
+ * -----------------------------------------------------------------------------
+ * module_text_normalizer.c 
+ *
+ * Usage:
+ *     Called from the main application or test modules to normalize text.
+ *
+ * Status:
+ *     Initial development — logic to handle text normalizing is not yet implemented.
+ *
+ * Author: [Anna Vasileva]
+ * -----------------------------------------------------------------------------
+ */
+
+#include "./module_text_normalizer.h"
+
+int text_normalizer(int n) {
+    printf("Text Normalizer module, still to be implemented\n");
+}
---
+++ src/module_text_normalizer/module_text_normalizer.h
@@ -0,0 +1,9 @@
+#ifndef MODULE_TEXT_NORMALIZER_H
+#define MODULE_TEXT_NORMALIZER_H
+
+#include "../main.h"
+
+int text_normalizer(int n);
+
+#endif
+ 
