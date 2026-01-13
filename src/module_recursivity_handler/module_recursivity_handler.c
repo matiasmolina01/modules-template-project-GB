1bf127f495347c3519ab7dc4dad9077f609df329
alexruiz0 <167028353+alexruiz0@users.noreply.github.com>
2026-01-13 20:49:42 +0100

    added recursivity handler module

---
+++ src/module_recursivity_handler/module_recursivity_handler.c
@@ -0,0 +1,19 @@
+/*
+ * -----------------------------------------------------------------------------
+ * module_recursivity_handler.c 
+ *
+ * Usage:
+ *     Called from the main application or test modules to handle recursivity in modules.
+ *
+ * Status:
+ *     Initial development — logic to handle recursivity is not yet implemented.
+ *
+ * Author: [Alex Ruiz]
+ * -----------------------------------------------------------------------------
+ */
+
+#include "./module_recursivity_handler.h"
+
+int recursivity_handler(int n) {
+    printf("Recursivity Handler module, still to be implemented\n");
+}
---
+++ src/module_recursivity_handler/module_recursivity_handler.h
@@ -0,0 +1,9 @@
+#ifndef MODULE_RECURSIVITY_HANDLER_H
+#define MODULE_RECURSIVITY_HANDLER_H
+
+#include "../main.h"
+
+int recursivity_handler(int n);
+
+#endif
+ 
