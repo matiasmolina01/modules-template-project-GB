d9eab03d00b984e68088dcb91815f902523403e6
martiiponsaa <marti.ponsa01@estudiant.upf.edu>
2026-01-14 00:49:31 +0100

    added subdirectories for compiling

---
+++ src/module_symbol_resolver/module_symbol_resolver.c
@@ -0,0 +1,20 @@
+/*
+ * -----------------------------------------------------------------------------
+ * module_symbol_resolver.c 
+ *
+ * Usage:
+ *     Called from the main application or test modules to process symbol resolver.
+ *
+ * Status:
+ *     Initial development — logic to handle symbol resolver is not yet implemented.
+ *
+ * Author: [Marc Lounds]
+ * -----------------------------------------------------------------------------
+ */
+
+
+#include "./module_symbol_resolver.h"
+
+int symbol_resolver(int n) {
+    printf("Symbol Resolver module, still to be implemented\n");
+}
---
+++ src/module_symbol_resolver/module_symbol_resolver.h
@@ -0,0 +1,9 @@
+#ifndef MODULE_SYMBOL_RESOLVER_H
+#define MODULE_SYMBOL_RESOLVER_H
+
+#include "../main.h"
+
+int symbol_resolver(int n);
+
+#endif
+ 
