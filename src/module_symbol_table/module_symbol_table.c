8da63a4c2bed63bf5978084c2a0d427a6f6e2aae
martiiponsaa <marti.ponsa01@estudiant.upf.edu>
2026-01-13 20:38:54 +0100

    author for module_symbols_table

--- src/module_symbol_table/module_symbol_table.c
+++ src/module_symbol_table/module_symbol_table.c
@@ -1,3 +1,17 @@
+/*
+ * -----------------------------------------------------------------------------
+ * module_symbol_table.c 
+ *
+ * Usage:
+ *     Called from the main application or test modules to process symbol table.
+ *
+ * Status:
+ *     Initial development — logic to handle symbol table is not yet implemented.
+ *
+ * Author: [Martí Ponsa]
+ * -----------------------------------------------------------------------------
+ */
+
 #include "./module_symbol_table.h"
 
 int symbol_table(int n) {
