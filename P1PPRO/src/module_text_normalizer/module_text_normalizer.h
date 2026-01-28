5869127f60a9f2df99b5e170aecc0a217a1361dd
matiasmolina01 <matiasezequiel.molina01@estudiant.upf.edu>
2026-01-28 15:14:56 +0100

    template P2LEX

---
+++ P1PPRO/src/module_text_normalizer/module_text_normalizer.h
@@ -0,0 +1,23 @@
+#ifndef MODULE_TEXT_NORMALIZER_H
+#define MODULE_TEXT_NORMALIZER_H
+
+#include "../main.h"
+
+typedef struct TextNormalizerState{
+    int in_block_comment;  // 0 = outside, 1 = inside /* */
+    int in_line_comment;  // 0 = code line, 1 = after //
+} TextNormalizerState;
+
+typedef struct ReplaceFlags ReplaceFlags;
+
+void text_normalizer_init(
+    TextNormalizerState* state
+); 
+
+char* text_normalizer(
+    const char* input_line,
+    TextNormalizerState* state,
+    ReplaceFlags *replace_flags
+);
+
+#endif
