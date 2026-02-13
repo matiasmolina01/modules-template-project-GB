faefd1c19431b1005a0cfaf50d0041ec240afb1f
martiiponsaa <marti.ponsa01@estudiant.upf.edu>
2026-02-13 12:57:14 +0100

    tests for the token module implemented and working

---
+++ P2LEX/tests/test_module_token_handler.h
@@ -0,0 +1,8 @@
+#ifndef TEST_MODULE_TOKEN_HANDLER_H
+#define TEST_MODULE_TOKEN_HANDLER_H
+ 
+#include "../tests/test_modules.h"
+#include "../src/token_handler/token_handler.h"
+void test_token_handler_run_all(void);
+
+#endif
