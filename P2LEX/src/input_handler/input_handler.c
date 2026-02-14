1f363afefde1391d36114eeaa98da6f43679c328
marc lounds <marcstriker99@gmail.com>
2026-02-14 12:30:46 +0100

    función reaf_char

--- P2LEX/src/input_handler/input_handler.c
+++ P2LEX/src/input_handler/input_handler.c
@@ -8,13 +8,32 @@
  * Status:
  *     Initial development — logic to handle input is not yet implemented.
  *
- * Author: []
+ * Author: Marc Lounds
  * -----------------------------------------------------------------------------
  */
 
 #include "./input_handler.h"
 
+//Para leer carácter por carácter
 
-void i_input_handler() {
-    fprintf(stdout, "Input handler module is under development.\n");
+int ioh_read_char(ioh_state_t *g_ioh) {
+    int c;
+
+    if (g_ioh->input_file == NULL) {
+        fprintf(stderr, "ioh_read_char: input file not opened\n");
+        return -1;
+    }
+
+    c = fgetc(g_ioh->input_file);
+
+    if (c == EOF) {
+        g_ioh->is_eof = true;
+        return 0;   // indica EOF
+    }
+
+    if (c == '\n') {
+        g_ioh->line_number++;
+    }
+
+    return c;  // return del carácter 
 }
