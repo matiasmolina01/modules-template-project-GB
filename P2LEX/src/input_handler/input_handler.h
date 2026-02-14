814e53fa241a1a563137d5be5c6566a3b19a40dc
marc lounds <marcstriker99@gmail.com>
2026-02-14 13:11:03 +0100

    estrutura de datos

--- P2LEX/src/input_handler/input_handler.h
+++ P2LEX/src/input_handler/input_handler.h
@@ -2,7 +2,16 @@
 #define MODULE_INPUT_HANDLER_H
 
 #include "../main.h"
+#include <stdio.h>
+#include <stdbool.h>
 
-void i_input_handler();
+
+typedef struct {
+    FILE *input_file;   // fichero de entrada
+    bool  is_eof;       // flag de fin de fichero
+    int   line_number;  // número de línea actual
+} ioh_state_t;
+
+void ioh_read_char(ioh_state_t *g_ioh);
 
 #endif
