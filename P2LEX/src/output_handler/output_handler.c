c5f64087aa7ea64422ac7f5d7eac45721e671a1e
ath3n4 <april.anna.vas@gmail.com>
2026-02-16 13:12:40 +0100

    Fixed output handler

--- P2LEX/src/output_handler/output_handler.c
+++ P2LEX/src/output_handler/output_handler.c
@@ -14,42 +14,24 @@
 
 #include "./output_handler.h"
 
-// This function doesn't need to exist, because token module already 
-// provides the necessary fucntions to print the token list in the appropiate way.
-static void write_token(FILE *fp, Token *tok) {
-    #if OUTFORMAT == RELEASE
-        // Should call t_token_print_release
-        fprintf(fp, "%s\n", tok->lexeme);
-    #else
-        // Should call t_token_print_debug
-        fprintf(fp,
-            "LEXEME: %-15s | CATEGORY: %-10s | LINE: %d | COL: %d\n",
-            tok->lexeme,
-            token_category_to_string(tok->category),
-            tok->line,
-            tok->column
-        );
-    #endif
-}
-
-// This function should call tl_token_list_print_release if in RELEASE mode, or tl_token_list_print_debug if in DEBUG mode, 
-// to print the entire list of tokens to the specified file. 
 void o_output_handler(TokenList *list, const char *filename) {
         if (!list || !filename) return -1;
 
     FILE *fp = fopen(filename, "w");
     if (!fp) return -1;
 
-    // Here 
     Token *node = token_list_head(list);
     while (node) {
         Token *tok = token_node_get(node);
         if (tok) {
-            write_token(fp, tok);
-        }
+                #if OUTFORMAT == RELEASE
+                    t_token_print_release(fp, tok);
+                #else
+                    t_token_print_debug(fp, tok);
+                #endif
+                    }
         node = token_node_next(node);
     }
 
     fclose(fp);
-    // return 0; I comment this because output_handler is void
 }
--- P2LEX/src/output_handler/output_handler.h
+++ P2LEX/src/output_handler/output_handler.h
@@ -5,31 +5,10 @@
 
 #define RELEASE 0
 #define DEBUG 1
-// Usage: gcc -DOUTFORMAT=DEBUG or gcc -DOUTFORMAT=RELEASE
 
 #ifndef OUTFORMAT
 #define OUTFORMAT RELEASE
 #endif
-
-typedef struct Token Token;
-typedef struct TokenList TokenList;
-
-// /* TEMPORAL: minimal definitions so output_handler can access tok->fields */
-// typedef enum {
-//     TOK_UNKNOWN = 0
-// } TokenCategory;
-
-// typedef struct Token {
-//     char lexeme[126];
-//     TokenCategory category;
-//     int line;
-//     int column;
-//     struct Token *next;   /* if your list uses next pointers (optional) */
-// } Token;
-
-// typedef struct TokenList {
-//     Token *head;
-// } TokenList;
 
 void o_output_handler(TokenList *list, const char *filename);
 
