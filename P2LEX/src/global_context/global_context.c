0249988cb0d61a9025dab524de667c3acd3ffc25
FrancOlano <olanofranco1@gmail.com>
2026-02-16 18:04:41 +0100

    funcion init automata

--- P2LEX/src/global_context/global_context.c
+++ P2LEX/src/global_context/global_context.c
@@ -6,30 +6,55 @@
  *     Called from the main file or test modules to manipulate the datastructures on the GlobalContext.
  *
  * Status:
- *     Initial development — logic to handle init is not yet implemented.
+ *     Final.
  *
- * Author: [Franco Olano]
+ * Author: Franco Olano
  * -----------------------------------------------------------------------------
  */
 
 #include "./global_context.h"
 
-
+/*
+    Define automatas using macros delared in language.h
+    Parameters:
+            automata_list: list of automatas to be created
+*/
 void  gc_init_automatas(Automata* automata_list[]){
 
-    //TODO define automatas using macros delared in language.h
-    //TODO automata_list[OPERATOR_AUTOMATA] = a_create_automata(...)
-    //TODO automata_list[SPECIAL_CHAR_AUTOMATA] = a_create_automata(...)
-    //TODO automata_list[NUMBERS_AUTOMATA] = a_create_automata(...)
-    //TODO automata_list[KEYWORDS_AUTOMATA] = a_create_automata(...)
-    //TODO automata_list[LITERALS_AUTOMATA] = a_create_automata(...)
-    //TODO automata_list[IDENTIFIERS_AUTOMATA] = a_create_automata(...)
+    
+    automata_list[OPERATOR_AUTOMATA] = a_create_automata(NUM_SYMBOLS_OPERATOR_AUTOMATA, NUM_STATES_OPERATOR_AUTOMATA,
+                                                 NUM_SYMBOLS_OPERATOR_AUTOMATA, operators_alphabet, operators_transitions,
+                                                 START_STATE, START_STATE, operators_accepting_states, CAT_OPERATOR);
+
+    automata_list[SPECIAL_CHAR_AUTOMATA] = a_create_automata(NUM_SYMBOLS_SPECIAL_CHAR_AUTOMATA, NUM_STATES_SPECIAL_CHAR_AUTOMATA,
+                                                 NUM_SYMBOLS_SPECIAL_CHAR_AUTOMATA, special_char_alphabet, special_char_transitions,
+                                                 START_STATE, START_STATE, special_accepting_states, CAT_SPECIALCHAR);
+
+    automata_list[NUMBERS_AUTOMATA] = a_create_automata(NUM_SYMBOLS_NUMBERS_AUTOMATA, NUM_STATES_NUMBERS_AUTOMATA,
+                                                 NUM_SYMBOLS_NUMBERS_AUTOMATA, numbers_alphabet, numbers_transitions,
+                                                 START_STATE, START_STATE, numbers_accepting_state, CAT_NUMBER);
+
+    automata_list[KEYWORDS_AUTOMATA] = a_create_automata(NUM_SYMBOLS_KEYWORD_AUTOMATA, NUM_STATES_KEYWORD_AUTOMATA,
+                                                 NUM_SYMBOLS_KEYWORD_AUTOMATA, keyword_alphabet, keyword_transitions,
+                                                 START_STATE, START_STATE, keyword_accepting_states, CAT_KEYWORD);
+
+    automata_list[LITERALS_AUTOMATA] = a_create_automata(NUM_SYMBOLS_LITERALS_AUTOMATA, NUM_STATES_LITERALS_AUTOMATA,
+                                                 NUM_SYMBOLS_LITERALS_AUTOMATA, literals_alphabet, literals_transitions,
+                                                 START_STATE, START_STATE, literals_accepting_state, CAT_LITERAL);
+
+    automata_list[IDENTIFIERS_AUTOMATA] = a_create_automata(NUM_SYMBOLS_IDENTIFIERS_AUTOMATA, NUM_STATES_IDENTIFIERS_AUTOMATA,
+                                                 NUM_SYMBOLS_IDENTIFIERS_AUTOMATA, identifiers_alphabet, identifiers_transitions,
+                                                 START_STATE, START_STATE, identifiers_accepting_state, CAT_IDENTIFIER);
 
 }
 
 /*
     Initilizes the GlobalContext datastructure.
-    Returns GlobalContext pointer.
+    Parameters:
+        arguments: previously initialized arguments data structure
+
+    Returns: 
+        initialized GlobalContext pointer.
 */
 GlobalContext* gc_init(Arguments* arguments) {
     GlobalContext *global_context = (GlobalContext*) malloc(sizeof(GlobalContext));
@@ -42,7 +67,7 @@ GlobalContext* gc_init(Arguments* arguments) {
     global_context->token_list = token_list;
 
     Automata* automatas_list[NUM_AUTOMATAS] = {};
-    //gc_init_automatas(automatas_list);
+    gc_init_automatas(automatas_list);
 
     Input* input = i_create();
 
@@ -53,10 +78,16 @@ GlobalContext* gc_init(Arguments* arguments) {
     return global_context;
 }
 
+
+/*
+    Util function to free the memory space of the global context
+    Parameters:
+        global_context: GlobalContext to be destroyed.
+*/
 void gc_destroy(GlobalContext* global_context){
-    //TODO fix
 	i_close_input(global_context->input);
-    free(global_context->token_list);
+    tl_token_list_destroy(global_context->token_list);
+    t_token_destroy(global_context->current_token);
     args_free(global_context->arguments);
     free(global_context);
 }
