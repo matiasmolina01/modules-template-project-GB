9758a18444c5bfbd182594b28dc245cb0ee47517
ath3n4 <april.anna.vas@gmail.com>
2026-03-05 18:15:37 +0100

    Started Parser

---
+++ P3PAR/src/parser/parser.c
@@ -0,0 +1,15 @@
+/*
+ * -----------------------------------------------------------------------------
+ * parser.c 
+ *
+ * Usage:
+ *     Called from the global context module to parse TokenList using the SRA.
+ *
+ * Status:
+ *     In progress.
+ *
+ * Author: Anna Vasileva
+ * -----------------------------------------------------------------------------
+ */
+
+#include "./parser.h"
