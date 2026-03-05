b00a133f571e0f710ef74f4bc05e433f01d93e43
ath3n4 <april.anna.vas@gmail.com>
2026-03-05 22:10:08 +0100

    Added parser main flow

--- P3PAR/src/parser/parser.c
+++ P3PAR/src/parser/parser.c
@@ -30,3 +30,70 @@ void parser_destroy(Parser* parser){
     if(!parser) return;
     free(parser);
 }
+
+Symbol parser_token_to_symbol(Token token)
+{
+    Symbol sym;
+
+    sym.name = token.lexeme;
+    sym.is_terminal = 1;
+
+    if (token.category == CAT_NUMBER)
+        sym.id = SYM_NUM;
+
+    else if (token.category == CAT_OPERATOR)
+    {
+        if (strcmp(token.lexeme, "+") == 0)
+            sym.id = SYM_PLUS;
+
+        else if (strcmp(token.lexeme, "*") == 0)
+            sym.id = SYM_STAR;
+    }
+
+    else if (token.category == CAT_SPECIALCHAR)
+    {
+        if (strcmp(token.lexeme, "(") == 0)
+            sym.id = SYM_LPAREN;
+
+        else if (strcmp(token.lexeme, ")") == 0)
+            sym.id = SYM_RPAREN;
+    }
+
+    return sym;
+}
+
+int parser_run(Parser* parser){
+
+    if(!parser || !parser->SRA || !parser->token)
+        return -1;
+
+    TokenNode* node = parser->token->head;
+
+    while(node){
+
+        Token* tok = &node->token;
+
+        Symbol sym = parser_token_to_symbol(*tok);
+
+        int action = sra_action(parser->SRA, sym);
+
+        if(action == ACT_SHIFT){
+            node = node->next;
+        }
+
+        else if(action == ACT_REDUCE){
+            continue;
+        }
+
+        else if(action == ACT_ACCEPT){
+            return 1;
+        }
+
+        else if(action == ACT_ERROR){
+            printf("Parser error");
+            return -1;
+        }
+    }
+
+    return 0;
+}
--- P3PAR/src/parser/parser.h
+++ P3PAR/src/parser/parser.h
@@ -8,8 +8,24 @@ typedef struct Parser {
     TokenList* token;
 } Parser;
 
+typedef enum {
+    SYM_S = 0,
+    SYM_LPAREN,
+    SYM_RPAREN,
+    SYM_PLUS,
+    SYM_STAR,
+    SYM_NUM,
+    SYM_EOF,
+
+    SYM_e,
+    SYM_t,
+    SYM_f
+
+} SymbolID;
+
 Parser* parser_create(SRA* sra, TokenList* tokens);
 void parser_destroy(Parser* parser);
+Symbol parser_token_to_symbol(Token token);
 int parser_run(Parser* parser);
 
 #endif
